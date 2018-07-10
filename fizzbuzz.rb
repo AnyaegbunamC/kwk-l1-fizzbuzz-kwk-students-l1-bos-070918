cde2981e7bc3b17201e0fd0e2fa23d2af047936b
Celine Anyaegbunam <cea03@icloud.com>
2018-07-10 20:17:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  end 
-end
+# def fizzbuzz(int)
+#   if int % 3 == 0
+#     "Fizz"
+#   end 
+# end
 
-puts fizzbuzz (3)
-puts fizzbuzz (9)
+# puts fizzbuzz (3)
+# puts fizzbuzz (9)
 
 
 
